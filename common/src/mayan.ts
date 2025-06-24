8988b5aa8b28b56457fd9dfd01d4eacf51392ec2
Quantum <quantum2048@gmail.com>
2025-06-23 20:29:26 -0400

    common: convert mayan.ts to 4 space indent for consistency

--- common/src/mayan.ts
+++ common/src/mayan.ts
@@ -1,96 +1,96 @@
 export enum TzolkinName {
-  IMIX,
-  IK,
-  AKBAL,
-  KAN,
-  CHIKCHAN,
-  KIMI,
-  MANIK,
-  LAMAT,
-  MULUK,
-  OK,
-  CHUWEN,
-  EB,
-  BEN,
-  IX,
-  MEN,
-  KIB,
-  KABAN,
-  ETZNAB,
-  KAWAK,
-  AJAW
+    IMIX,
+    IK,
+    AKBAL,
+    KAN,
+    CHIKCHAN,
+    KIMI,
+    MANIK,
+    LAMAT,
+    MULUK,
+    OK,
+    CHUWEN,
+    EB,
+    BEN,
+    IX,
+    MEN,
+    KIB,
+    KABAN,
+    ETZNAB,
+    KAWAK,
+    AJAW
 }
 
 export type TzolkinNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13;
 
 export type Tzolkin = {
-  number: TzolkinNumber,
-  name: TzolkinName,
+    number: TzolkinNumber,
+    name: TzolkinName,
 };
 
 const tzolkinNames: Record<TzolkinName, string> = {
-  [TzolkinName.IMIX]: 'Imix',
-  [TzolkinName.IK]: 'Ikʼ',
-  [TzolkinName.AKBAL]: 'Akʼbʼal',
-  [TzolkinName.KAN]: 'Kʼan',
-  [TzolkinName.CHIKCHAN]: 'Chikchan',
-  [TzolkinName.KIMI]: 'Kimi',
-  [TzolkinName.MANIK]: 'Manikʼ',
-  [TzolkinName.LAMAT]: 'Lamat',
-  [TzolkinName.MULUK]: 'Muluk',
-  [TzolkinName.OK]: 'Ok',
-  [TzolkinName.CHUWEN]: 'Chuwen',
-  [TzolkinName.EB]: 'Ebʼ',
-  [TzolkinName.BEN]: 'Bʼen',
-  [TzolkinName.IX]: 'Ix',
-  [TzolkinName.MEN]: 'Men',
-  [TzolkinName.KIB]: 'Kibʼ',
-  [TzolkinName.KABAN]: 'Kabʼan',
-  [TzolkinName.ETZNAB]: 'Etzʼnabʼ',
-  [TzolkinName.KAWAK]: 'Kawak',
-  [TzolkinName.AJAW]: 'Ajaw',
+    [TzolkinName.IMIX]: 'Imix',
+    [TzolkinName.IK]: 'Ikʼ',
+    [TzolkinName.AKBAL]: 'Akʼbʼal',
+    [TzolkinName.KAN]: 'Kʼan',
+    [TzolkinName.CHIKCHAN]: 'Chikchan',
+    [TzolkinName.KIMI]: 'Kimi',
+    [TzolkinName.MANIK]: 'Manikʼ',
+    [TzolkinName.LAMAT]: 'Lamat',
+    [TzolkinName.MULUK]: 'Muluk',
+    [TzolkinName.OK]: 'Ok',
+    [TzolkinName.CHUWEN]: 'Chuwen',
+    [TzolkinName.EB]: 'Ebʼ',
+    [TzolkinName.BEN]: 'Bʼen',
+    [TzolkinName.IX]: 'Ix',
+    [TzolkinName.MEN]: 'Men',
+    [TzolkinName.KIB]: 'Kibʼ',
+    [TzolkinName.KABAN]: 'Kabʼan',
+    [TzolkinName.ETZNAB]: 'Etzʼnabʼ',
+    [TzolkinName.KAWAK]: 'Kawak',
+    [TzolkinName.AJAW]: 'Ajaw',
 };
 
 export function tzolkinName(name: TzolkinName): string {
-  return tzolkinNames[name];
+    return tzolkinNames[name];
 }
 
 export function formatTzolkin(tzolkin: Tzolkin): string {
-  return `${tzolkin.number} ${tzolkinName(tzolkin.name)}`;
+    return `${tzolkin.number} ${tzolkinName(tzolkin.name)}`;
 }
 
 export function jdnTzolkin(jdn: number): Tzolkin {
-  return {
-    number: ((jdn % 13 + 18) % 13 + 1) as TzolkinNumber,
-    name: (jdn % 20 + 36) % 20,
-  };
+    return {
+        number: ((jdn % 13 + 18) % 13 + 1) as TzolkinNumber,
+        name: (jdn % 20 + 36) % 20,
+    };
 }
 
 export type HaabMonth = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19;
 export type HaabDay = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19;
 
 const haabNames: Record<HaabMonth, string> = {
-  1: 'Pop',
-  2: 'Woʼ',
-  3: 'Sip',
-  4: 'Sotzʼ',
-  5: 'Sek',
-  6: 'Xul',
-  7: 'Yaxkʼin',
-  8: 'Mol',
-  9: 'Chʼen',
-  10: 'Yax',
-  11: 'Sakʼ',
-  12: 'Keh',
-  13: 'Mak',
-  14: 'Kʼankʼin',
-  15: 'Muwan',
-  16: 'Pax',
-  17: 'Kʼayabʼ',
-  18: 'Kumkʼu',
-  19: 'Wayebʼ',
+    1: 'Pop',
+    2: 'Woʼ',
+    3: 'Sip',
+    4: 'Sotzʼ',
+    5: 'Sek',
+    6: 'Xul',
+    7: 'Yaxkʼin',
+    8: 'Mol',
+    9: 'Chʼen',
+    10: 'Yax',
+    11: 'Sakʼ',
+    12: 'Keh',
+    13: 'Mak',
+    14: 'Kʼankʼin',
+    15: 'Muwan',
+    16: 'Pax',
+    17: 'Kʼayabʼ',
+    18: 'Kumkʼu',
+    19: 'Wayebʼ',
 };
 
 export function formatHaab(month: HaabMonth, day: HaabDay): string {
-  return `${day} ${haabNames[month]}`;
+    return `${day} ${haabNames[month]}`;
 }
